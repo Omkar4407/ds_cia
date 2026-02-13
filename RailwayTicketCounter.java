4dc67013b5b71485642f30803d5cc01eef0610fe
Omkar4407 <omkarb4407@gmail.com>
2026-02-13 13:12:09 +0530

    Initial commit

--- RailwayTicketCounter.java
+++ RailwayTicketCounter.java
@@ -2,21 +2,22 @@ package cia;
 import java.util.*;
 
 public class RailwayTicketCounter {
+    // A Scanner object will be taking input from user
+	static Scanner sc = new Scanner(System.in);
 
-	    static Scanner sc = new Scanner(System.in);
+	// A Queue which will store the customers' waiting list for tickets (First in - First out method)
+	static Queue<String> customerQueue = new LinkedList<>();
 
-	    // Queue for customers
-	    static Queue<String> customerQueue = new LinkedList<>();
-
-	    // Stack for cancelled tickets
-	    static Stack<String> cancelledTickets = new Stack<>();
-
-	    // Array for ticket storage
-	    static String[] tickets = new String[100];
-	    static int ticketCount = 0;
+	// A Stack for storing cancelled tickets (Last in - First out method)
+	static Stack<String> cancelledTickets = new Stack<>();
+    
+	// An Array for storing the issued tickets
+	static String[] tickets = new String[100];
+	static int ticketCount = 0; // keeps the count of number of tickets issued
 
 	public static void main(String[] args) {
 		
+        // A loop which will display a menu in front of the user until the user hits exit
 		while (true) {
             System.out.println("\n--- Railway Ticket Counter ---");
             System.out.println("1. Add Customer");
@@ -25,9 +26,11 @@ public class RailwayTicketCounter {
             System.out.println("4. Display Waiting List");
             System.out.println("5. Exit");
 
+            // This reads the user's choice
             int choice = sc.nextInt();
             sc.nextLine();
 
+            // A switch case is used to perform the selected operation by the user
             switch (choice) {
                 case 1 -> addCustomer();
                 case 2 -> serveCustomer();
@@ -39,44 +42,57 @@ public class RailwayTicketCounter {
         }
     }
 
+    // adds a customer to the queue
     static void addCustomer() {
         System.out.print("Enter customer name: ");
-        String name = sc.nextLine();
+        String name = sc.nextLine(); // reads the customers' name
 
+        // adds the customer to the queue
         customerQueue.add(name);
         System.out.println("Customer added to queue!");
     }
 
+    // issues a ticket to the customer
     static void serveCustomer() {
+        // checks if there are no customers
         if (customerQueue.isEmpty()) {
             System.out.println("No customers!");
             return;
         }
 
+        // removes the customer from the queue who got the ticket issued
         String customer = customerQueue.poll();
+        // stored the issued ticket in array for cancellation later
         tickets[ticketCount++] = customer;
 
         System.out.println("Ticket issued to: " + customer);
     }
 
+    // cancels the last issued ticket
     static void cancelTicket() {
+        // checks if issued tickets exists
         if (ticketCount == 0) {
             System.out.println("No tickets to cancel!");
             return;
         }
 
+        // cancels the last issued ticket
         String cancelled = tickets[--ticketCount];
+        // pushes the cancelled ticket into the stack
         cancelledTickets.push(cancelled);
 
         System.out.println("Cancelled ticket of: " + cancelled);
     }
 
+    // displays the waiting customers' queue
     static void displayQueue() {
+        // checks if there are no waiting customers
         if (customerQueue.isEmpty()) {
             System.out.println("No waiting customers!");
             return;
         }
 
+        // iterates through the queue and displays all the customers in the waiting list
         System.out.println("Waiting Customers:");
         for (String c : customerQueue) {
             System.out.println("- " + c);
--- SmartTaskManager.java
+++ SmartTaskManager.java
@@ -3,21 +3,22 @@ package cia;
 import java.util.*;
 
 public class SmartTaskManager {
-
+    // A Scanner object will be taking input from user
 	static Scanner sc = new Scanner(System.in);
 
-	// Array to store all tasks
+	// An Array to store the tasks entered by the user
 	static String[] allTasks = new String[100];
 	static int taskCount = 0;
 
-	// Queue for pending tasks
+	// A Queue which will store the pending tasks of the user (First in - First out method)
 	static Queue<String> taskQueue = new LinkedList<>();
     
-	// Stack for undo
+	// This stack will store processed task which be later useful for undo of operations (Last in - First out method)
 	static Stack<String> undoStack = new Stack<>();
 
 	public static void main(String[] args) {
 		
+        // A loop which will display a menu in front of the user until the user hits exit
 		while (true) {
             System.out.println("\n--- Smart Task Management ---");
             System.out.println("1. Add Task");
@@ -26,9 +27,11 @@ public class SmartTaskManager {
             System.out.println("4. Display Pending Tasks");
             System.out.println("5. Exit");
 
+            // This reads the user's choice
             int choice = sc.nextInt();
             sc.nextLine();
 
+            // A switch case is used to perform the selected operation by the user
             switch (choice) {
                 case 1 -> addTask();
                 case 2 -> processTask();
@@ -39,47 +42,55 @@ public class SmartTaskManager {
             }
         }
     }
-
+    // Adds a new task
     static void addTask() {
         System.out.print("Enter task name: ");
         String task = sc.nextLine();
 
-        allTasks[taskCount++] = task;
-        taskQueue.add(task);
+        allTasks[taskCount++] = task; // stores the task in the array
+        taskQueue.add(task);  // adds the task to queue for processing later
 
         System.out.println("Task added successfully!");
     }
-
+    // processes a task
     static void processTask() {
+        // checks if there are no pending tasks
         if (taskQueue.isEmpty()) {
             System.out.println("No pending tasks!");
             return;
         }
 
+        // removes task from the queue (FIFO rule)
         String task = taskQueue.poll();
+        // pushes the processed task into the stack for undo operation later (LIFO rule)
         undoStack.push(task);
 
         System.out.println("Processed Task: " + task);
     }
-
+    // undo the last processed task
     static void undoTask() {
+        // checks if the stack is empty for undo operation
         if (undoStack.isEmpty()) {
             System.out.println("Nothing to undo!");
             return;
         }
-
+        
+        // pops the last processed task
         String task = undoStack.pop();
+        // adds it back to the queue
         taskQueue.add(task);
 
         System.out.println("Undo successful: " + task);
     }
-
+    // displays all pending tasks
     static void displayTasks() {
+        // checks if all tasks are processed
         if (taskQueue.isEmpty()) {
             System.out.println("No pending tasks!");
             return;
         }
 
+        // iterates through the queue and prints all the pending tasks
         System.out.println("Pending Tasks:");
         for (String t : taskQueue) {
             System.out.println("- " + t);
